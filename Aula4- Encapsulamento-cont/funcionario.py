79da59e8dbce4ea798ee4778ffeaeb5286941d0f
Thaynara Barcelos <vitoriathaynara63@gmail.com>
2024-10-02 17:00:57 -0300

    Trabalhando com Encapsulameno- Cont

---
+++ Aula4- Encapsulamento-cont/funcionario.py
@@ -0,0 +1,25 @@
+class Funcionario:
+    def __init__(self, nome, salario = 1000):
+        self.__nome = nome
+        self.__salario = salario
+
+    def getNome(self):
+        return self.__nome
+    
+    def setNome(self, valor):
+        self.__nome = valor #Alterar
+
+
+    #Iremos utilizar um recurso único do python para acessar atributos privados
+    
+    #Criando um get personalizado
+    @property #esse item irá criar um hget "mais amigável"
+    def salario(self):
+        return self.__salario
+    
+    @salario.setter #Irá criar um set para o atributo de forma  "mais amigável"
+    def salario(self, valor):
+        if valor >0:
+            self.__salario = valor 
+        else:
+            print("Você não pode ter un salário negativo")
